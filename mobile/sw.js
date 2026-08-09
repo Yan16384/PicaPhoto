33f43000fd5d19995c9cfad34409b588309bcc80
Yan16384 <271541763+Yan16384@users.noreply.github.com>
2026-08-09 12:50:01 +0800

    Add PicaPhoto Mobile sw.js

---
+++ mobile/sw.js
@@ -0,0 +1,21 @@
+﻿const CACHE = "picaphoto-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    "./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"
+  ])).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      if (e.request.url.startsWith(self.location.origin) && !e.request.url.includes("blob:")) {
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
